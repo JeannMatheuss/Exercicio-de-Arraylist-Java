c9cb84f39ea8b48e140e2bbcfbb11c952e98d8e8
Jean <jeanmatheus0612@gmail.com>
2024-12-28 18:58:49 -0300

    Criando as pessoas

--- src/Principal.java
+++ src/Principal.java
@@ -7,5 +7,17 @@ public class Principal {
 
         // criando a lista de pessoas com a classe Pessoa entre isso <> 
         ArrayList<Pessoa> listaDePessoas = new ArrayList<>();
+
+        Pessoa pessoa1 = new Pessoa();
+        pessoa1.setNome("Jean");
+        pessoa1.setIdade(19);
+
+        Pessoa pessoa2 = new Pessoa();
+        pessoa2.setNome("Márcia");
+        pessoa2.setIdade(12);
+
+        Pessoa pessoa3 = new Pessoa();
+        pessoa3.setNome("João");
+        pessoa3.setIdade(25);
     }
 }
