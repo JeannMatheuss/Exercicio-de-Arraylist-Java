d5b63f2b1ddf417f6ef7f1fd22c3b369abc4200e
Jean <jeanmatheus0612@gmail.com>
2024-12-28 18:56:11 -0300

    Criando o arraylist

--- src/Principal.java
+++ src/Principal.java
@@ -1,3 +1,11 @@
+import java.util.ArrayList;
+
+import br.com.exercicio.modelos.Pessoa;
+
 public class Principal {
-    
+    public static void main(String[] args) {
+
+        // criando a lista de pessoas com a classe Pessoa entre isso <> 
+        ArrayList<Pessoa> listaDePessoas = new ArrayList<>();
+    }
 }
