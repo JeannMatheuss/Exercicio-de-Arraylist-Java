274726af51b10f73d0d4506058efab148b334ac9
Jean <jeanmatheus0612@gmail.com>
2024-12-30 02:02:02 -0300

    Criando a subclasse e o construtor

---
+++ src/br/com/exercicio/modelos1/ProdutoPerecivel.java
@@ -0,0 +1,8 @@
+package br.com.exercicio.modelos1;
+
+public class ProdutoPerecivel extends Produtos {
+    
+    public ProdutoPerecivel (String nome, double preco, int quantidade) {
+        super(nome, preco, quantidade);
+    }
+}
