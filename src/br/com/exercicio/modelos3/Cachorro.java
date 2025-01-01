c8b6fd52a81a1a0d1a0d3b16cdad087a140524e5
Jean <jeanmatheus0612@gmail.com>
2025-01-01 19:48:41 -0300

    Criando os atributos e metodos getters e setters

--- src/br/com/exercicio/modelos3/Cachorro.java
+++ src/br/com/exercicio/modelos3/Cachorro.java
@@ -1,5 +1,15 @@
 package br.com.exercicio.modelos3;
 
 public class Cachorro extends Animal {
+    private String raca;
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
     
 }
