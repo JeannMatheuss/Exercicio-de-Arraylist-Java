68689a18cf732b7091fdb1126ea8fd6754fe68ba
Jean <jeanmatheus0612@gmail.com>
2025-01-01 19:53:10 -0300

    Colocando o nome e a raça do cachorro

--- src/Principal3.java
+++ src/Principal3.java
@@ -3,5 +3,7 @@ import br.com.exercicio.modelos3.Cachorro;
 public class Principal3 {
     public static void main(String[] args) {
         Cachorro cachorro = new Cachorro();
+        cachorro.setNome("Bela");
+        cachorro.setRaca("Poodle");
     }
 }
