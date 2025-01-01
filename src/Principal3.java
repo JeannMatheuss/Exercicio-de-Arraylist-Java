63631057814ea725a4fb515cf95ea204d33d6cd4
Jean <jeanmatheus0612@gmail.com>
2025-01-01 19:52:19 -0300

    Criando o objeto cachorro

--- src/Principal3.java
+++ src/Principal3.java
@@ -1,5 +1,7 @@
+import br.com.exercicio.modelos3.Cachorro;
+
 public class Principal3 {
     public static void main(String[] args) {
-        
+        Cachorro cachorro = new Cachorro();
     }
 }
