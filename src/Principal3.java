c4df7d9e7b8f2f6b108fd63db6436064ff448500
Jean <jeanmatheus0612@gmail.com>
2025-01-02 20:31:24 -0300

    Fazendo o casting das classes Animal e Cachorro

--- src/Principal3.java
+++ src/Principal3.java
@@ -1,9 +1,9 @@
+import br.com.exercicio.modelos3.Animal;
 import br.com.exercicio.modelos3.Cachorro;
 
 public class Principal3 {
     public static void main(String[] args) {
         Cachorro cachorro = new Cachorro();
-        cachorro.setNome("Bela");
-        cachorro.setRaca("Poodle");
+        Animal animal = (Animal) cachorro;
     }
 }
