727052e34e15990c1dc8af43df71422d4b175d78
Jean <jeanmatheus0612@gmail.com>
2025-01-02 20:35:11 -0300

    Fazendo a verificação com instanceof

--- src/Principal3.java
+++ src/Principal3.java
@@ -5,5 +5,12 @@ public class Principal3 {
     public static void main(String[] args) {
         Cachorro cachorro = new Cachorro();
         Animal animal = (Animal) cachorro;
+
+        Animal animalCachorro = new Cachorro();
+        if (animal instanceof Cachorro) {
+            Cachorro cachorroAnimal = (Cachorro) animal;
+        } else {
+            System.out.println("O objeto não é um Cachorro.");
+        }
     }
 }
